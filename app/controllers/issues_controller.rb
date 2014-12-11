8b7d827bdf99389750d3fef65c1743232dd96280
EduRomoGO <eduromogo@outlook.com>
2014-12-11 16:14:40 +0100

    First version of issues page. No logic behind form. Issues page is available to see the list of issues opened by the user, and targeting the user. No logic is performed to get the button send to work. Closes-Issue: #2

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,12 +1,12 @@
 class IssuesController < ApplicationController
 
 	def index
-
+		@issue = Issue.new
 		@issues_targeted = Issue.where(target_user_id: params[:user_id])
 		begin
 			#binding.pry
-			user = User.find params[:user_id]
-			if(user.issues.count > 0)
+			@user = User.find params[:user_id]
+			if(@user.issues.count > 0)
 					@issues = User.first.issues
 			else
 					@issues = []		
--- spec/features/issues_spec.rb
+++ spec/features/issues_spec.rb
@@ -1,59 +1,59 @@
 require_relative '../rails_helper'
 
-describe "user issues page", :type => :feature do
+describe "User issues page", :type => :feature do
 
-  it "shows the page" do
+	let(:u) {User.create(:identification_type => 'nif',
+    					:identification_number => '12345678A',
+    					:plate => '1234AAA', 
+    					:extra_plates => 'false')}
+
+ 	let(:create_test_issues_for_user) {Issue.create(:user_id => u.id,
+																							 :text => 'park issue',
+																							 :target_user_id => 2)
+																	   Issue.create(:user_id => u.id,
+																							 :text => 'park issue new',
+																							 :target_user_id => 3)}
+
+ 	let(:create_test_issues_to_target_user) {Issue.create(:user_id => 1,
+																										 :text => 'park issue',
+																										 :target_user_id => u.id)
+																			    Issue.create(:user_id => 2,
+																										 :text => 'park issue new',
+																										 :target_user_id => u.id)
+																			    Issue.create(:user_id => 1,
+																										 :text => 'park issue newer',
+																										 :target_user_id => u.id)}
+
+  xit "shows itself when visited" do
     visit '/users/1/issues'
   end
 
+=begin
   xit "capybara test" do
     visit '/users/1/issues'
     page.should have_xpath('//p')
     expect(all('p').count).to be(1)
   end
+=end
+
 
   it "shows the issues list where user is owner" do
-    u = User.create(:identification_type => 'nif',
-    					:identification_number => '12345678A',
-    					:plate => '1234AAA', 
-    					:extra_plates => 'false')
 
-    Issue.create(:user_id => u.id,
-							 :text => 'park issue',
-							 :target_user_id => 2)
-    Issue.create(:user_id => u.id,
-							 :text => 'park issue new',
-							 :target_user_id => 3)
+  	create_test_issues_for_user
 
     visit "/users/#{u.id}/issues"
     expect(all('article').count).to be(2)
   end
 
   it "shows the issues list where user is the target of the issue" do
-    u = User.create(:identification_type => 'nif',
-    					:identification_number => '12345678A',
-    					:plate => '1234AAA', 
-    					:extra_plates => 'false')
 
-    Issue.create(:user_id => 1,
-							 :text => 'park issue',
-							 :target_user_id => u.id)
-    Issue.create(:user_id => 2,
-							 :text => 'park issue new',
-							 :target_user_id => u.id)
-    Issue.create(:user_id => 1,
-							 :text => 'park issue newer',
-							 :target_user_id => u.id)
+		create_test_issues_to_target_user
 
     visit "/users/#{u.id}/issues"
     expect(all('article').count).to be(3)
   end
 
   it "show info message if doesnt get any issues where the user is the owner" do
-    u = User.create(:identification_type => 'nif',
-    					:identification_number => '12345678A',
-    					:plate => '1234AAA', 
-    					:extra_plates => 'false')
 
     visit "/users/#{u.id}/issues"
     expect(all('article').count).to be(0)
@@ -62,15 +62,47 @@ describe "user issues page", :type => :feature do
   end
 
   it "show info message if doesnt get any issues where the user is targeted" do
-    u = User.create(:identification_type => 'nif',
-    					:identification_number => '12345678A',
-    					:plate => '1234AAA', 
-    					:extra_plates => 'false')
 
     visit "/users/#{u.id}/issues"
     expect(all('article').count).to be(0)
     expect(page.has_css?('p.empty_issues_targeted_message')).to be(true)
   end
 
+
+  describe "has a form to create a new issue", :type => :feature do
+
+	  it "that has a form tag" do
+
+	    visit "/users/#{u.id}/issues"
+	    expect(all('form').count).to be(1)
+	  end
+
+	  it "that has an input for the user plate" do
+
+	    visit "/users/#{u.id}/issues"
+	    expect(all('input#issue_target_user_id').count).to be(1)
+	  end
+
+	  it "where the input for the plate field is empty" do
+
+	    visit "/users/#{u.id}/issues"
+	    expect(find('input#issue_target_user_id').text).to eq("")
+	  end
+
+	  it "that has one and only one empty text input for the body of the the issue" do
+
+	    visit "/users/#{u.id}/issues"
+	    expect(all('input#issue_text').count).to be(1)
+	    expect(find('input#issue_text').text).to eq("")
+	  end
+
+	  it "that has one button to submit the form" do
+
+	    visit "/users/#{u.id}/issues"
+	    expect(all('input[name="commit"]').count).to be(1)
+	    expect(find('input[name="commit"]').value).to eq("Save")
+	  end
+
+	end
 
 end
